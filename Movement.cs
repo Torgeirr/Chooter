8c659183480a5d523a763449edb53374b02daea7
Torgeirr <jawallace1991@yahoo.com>
2017-12-19 18:08:20 -0700

    Generic Get Axis Movement
    Generalized Get-Axis based movement script

---
+++ Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float speed = 15f;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        //rigidbody.position - new Vector3()
+    }
+}
