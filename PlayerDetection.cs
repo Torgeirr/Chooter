70d3389feed132fdad66bff962dc0f053bb545a6
Torgeirr <jawallace1991@yahoo.com>
2017-12-19 18:31:09 -0700

    Player Detection
    Idk why the hell this won't work tbh but w/e

---
+++ PlayerDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour {
+
+    public Gameobject Npc;
+
+    public Vector3 distanceBetween;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceBetween = newVector3(Npc - gameobject.other);
+
+        if (distanceBetween <= 15) {
+            Debug.Log("Dialogue Box Popped Up");
+        }
+		
+	}
+}
