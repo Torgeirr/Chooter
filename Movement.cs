12addbb6b7547a1b5a072de74f3f579fe199d4aa
Torgeirr <jawallace1991@yahoo.com>
2017-12-21 12:49:44 -0700

    Movement Accuracy Update
    Parameters moveVertical and moveHorizontal swapped in line 23 to fix movement perpendicular movement, and a negative value added to moveVertical in order to correct backwards/mirrored movement.

---
+++ Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float speed = 15f;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-moveVertical, 0.0f, moveHorizontal);
+
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+    }
+}
