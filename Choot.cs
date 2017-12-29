f901478c803f02854d51627023f224e7088ec480
Torgeirr <jawallace1991@yahoo.com>
2017-12-29 15:02:42 -0700

    Choot with Velocity
    Don't make the mistake of thinking this works lol

--- Choot.cs
+++ Choot.cs
@@ -8,15 +8,30 @@ public class Choot : MonoBehaviour {
 
     public GameObject boolet;
 
+    public Rigidbody rb;
+
+    public bool booletChot = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
             GameObject.Instantiate(boolet, initial);
+
+            booletChot = true;
+
         }
-	}
+
+        if (booletChot == true)
+        {
+            rb.velocity = new Vector3(0, 0, 100);
+        }
+        else {
+            return;
+        }
+    }
 }
