4d056c503d7739d7e63fbaddcc21f7c9906e66dc
Torgeirr <jawallace1991@yahoo.com>
2017-12-29 14:29:17 -0700

    Basic Choot Script Added
    And I mean basic. Just instantiates on an object's transform, without velocity or anything.

---
+++ Choot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choot : MonoBehaviour {
+
+    public Transform initial;
+
+    public GameObject boolet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject.Instantiate(boolet, initial);
+        }
+	}
+}
